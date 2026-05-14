7802a2c9857a994956db5769afac30aa176f416f
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-05-14 09:52:35 -0600

    refactor: reorganized service structure

---
+++ services/accounts-service/src/main/java/org/dsa/services/accountsservice/AccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.dsa.services.accountsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsServiceApplication.class, args);
+    }
+
+}
---
+++ services/accounts-service/src/test/java/org/dsa/services/accountsservice/AccountsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.dsa.services.accountsservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+    @Test
+    @DisplayName("AccountService - Smoke test")
+    void contextLoads() {
+    }
+
+}
