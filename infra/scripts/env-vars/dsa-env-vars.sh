75c542b85452403e4702922b5df1142c265dd2d3
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-05-20 12:15:21 -0600

    compose dir renamed

--- infra/scripts/env-vars/dsa-env-vars.sh
+++ infra/scripts/env-vars/dsa-env-vars.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 export DSA_ROOT_DIR="$HOME/IdeaProjects/distributed-systems-architecture"
 export INFRA_DIR="$DSA_ROOT_DIR/infra"
-export COMPOSE_DIR="$INFRA_DIR/compose"
+export COMPOSES_DIR="$INFRA_DIR/composes"
 
-export SERVICES_COMPOSE_DIR="$COMPOSE_DIR/services"
-export TEAMCITY_COMPOSE_DIR="$COMPOSE_DIR/teamcity"
+export SERVICES_COMPOSE_DIR="$COMPOSES_DIR/services"
+export TEAMCITY_COMPOSE_DIR="$COMPOSES_DIR/teamcity"
 
 export TEAMCITY_SERVER_URL="http://localhost:8111"
