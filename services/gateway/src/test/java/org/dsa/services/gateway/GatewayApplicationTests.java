bd86d6d39e9a0580843b9ac019c0180e8a1db684
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-05-21 16:37:25 -0600

    typos fixed in files gitlab-ci

--- services/gateway/src/test/java/org/dsa/services/gateway/GatewayApplicationTests.java
+++ services/gateway/src/test/java/org/dsa/services/gateway/GatewayApplicationTests.java
@@ -1,16 +1,16 @@
 package org.dsa.services.gateway;
 
-import org.dsa.core.sharedstarter.testing.annotations.IntegrationEnvironment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
-//@IntegrationEnvironment
+@SpringBootTest
+@ActiveProfiles("dev")
 class GatewayApplicationTests {
 
-    //@Test
     @DisplayName("gateway smoke test")
+    @Test
     void contextLoads() {
     }
 
