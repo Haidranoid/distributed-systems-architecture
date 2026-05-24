4cf19221ba8b6f3b40feb120518cfce579a3017a
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-05-24 00:37:29 -0600

    infra scripts refactored 47

--- infra/scripts/env-vars.sh
+++ infra/scripts/env-vars.sh
@@ -2,11 +2,6 @@
 
 set -eu
 
-
-#export DOCKER_COMPOSES_DIR="$INFRA_DIR/docker-composes"
-#export K8S_DIR="$INFRA_DIR/k8s"
-#export SCRIPTS_DIR="$INFRA_DIR/scripts"
-
 if [ -n "${CI_PIPELINE_ID+x}" ]; then
   export GRADLE_USER_HOME="cache/gradle/$PROJECT_NAME"
   export PROJECT_IMAGE_TAG="$AWS_ECR_DSA_REPOSITORY_NAME/$PROJECT_NAME"
