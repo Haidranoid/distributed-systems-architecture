7d368727c99bce67bb4da0c5ccbfa8747442f2c9
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-07-07 12:21:08 -0600

    new set-env.sh files for future migration

--- infra/scripts/.temp_v2_setup-env.sh
+++ infra/scripts/.temp_v2_setup-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#set -eu
+set -eu
 
 PROJECT_ROOT="$(pwd)"
 
@@ -12,6 +12,6 @@ export KAFKA_COMPOSE_DIR="$PROJECT_ROOT/infra/docker-composes/kafka"
 export SERVICES_COMPOSE_FILE="$SERVICES_COMPOSE_DIR/docker-compose.yml"
 export KAFKA_COMPOSE_FILE="$KAFKA_COMPOSE_DIR/docker-compose.yml"
 
-. aws-utils.sh
-. docker-utils.sh
-. composes-utils.sh
+. "$PROJECT_ROOT/infra/scripts/aws-utils.sh"
+. "$PROJECT_ROOT/infra/scripts/docker-utils.sh"
+. "$PROJECT_ROOT/infra/scripts/composes-utils.sh"
