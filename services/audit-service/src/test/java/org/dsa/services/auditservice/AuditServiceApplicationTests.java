efe73da7057e260242469bb2d13ed1b15ac98c10
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-07-11 15:02:30 -0600

    shared-starter dependency updated to v1.4.1 in audit-service

--- services/audit-service/src/test/java/org/dsa/services/auditservice/AuditServiceApplicationTests.java
+++ services/audit-service/src/test/java/org/dsa/services/auditservice/AuditServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package org.dsa.services.auditservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AuditServiceApplicationTests {
 
     @Test
+    @DisplayName("audit-service smoke test")
     void contextLoads() {
+        //TODO: add validation at startup to check if Kafka is up
     }
 
 }
