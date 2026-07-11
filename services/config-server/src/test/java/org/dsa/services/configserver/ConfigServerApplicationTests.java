112714937f47f8ba3c096720beb88a0143e75811
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-07-11 15:07:54 -0600

    code format standarized in config-server

--- services/config-server/src/test/java/org/dsa/services/configserver/ConfigServerApplicationTests.java
+++ services/config-server/src/test/java/org/dsa/services/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
 package org.dsa.services.configserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConfigServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  @DisplayName("config-server smoke test")
+  void contextLoads() {}
 }
