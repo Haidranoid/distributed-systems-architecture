396689cf1346cbcbcc9a2100ad6903ca4f2cd627
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-07-12 12:38:44 -0600

    toString method overrided for responses

--- services/authentication-service/src/main/java/org/dsa/services/authenticationservice/response/AuthenticationResponse.java
+++ services/authentication-service/src/main/java/org/dsa/services/authenticationservice/response/AuthenticationResponse.java
@@ -1,7 +1,18 @@
 package org.dsa.services.authenticationservice.response;
 
+import com.google.common.base.MoreObjects;
 import lombok.Builder;
+import org.jspecify.annotations.NonNull;
 
 @Builder
 public record AuthenticationResponse(
-    String accessToken, String refreshToken, AccountResponse accountResponse) {}
+    String accessToken, String refreshToken, AccountResponse accountResponse) {
+  @Override
+  public @NonNull String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("accessToken", "[PROTECTED]")
+        .add("refreshToken", "[PROTECTED]")
+        .add("accountResponse", accountResponse)
+        .toString();
+  }
+}
