a15bac1eb87a6f84d52d5dbf583bb9c32afaa397
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-07-23 15:35:47 -0600

    distributed-systems-architecture dir structure updated - v11

--- infrastructure/scripts/load-dirs-vars.sh
+++ infrastructure/scripts/load-dirs-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export SERVICES_COMPOSE_DIR="$SCRIPTS_DIR/../composes/services"
-export KAFKA_COMPOSE_DIR="$SCRIPTS_DIR/../composes/kafka"
+export SERVICES_COMPOSE_DIR="$SCRIPTS_DIR/../docker-registry/composes/microservices"
+export KAFKA_COMPOSE_DIR="$SCRIPTS_DIR/../docker-registry/composes/kafka"
 
 export SERVICES_COMPOSE_FILE="$SERVICES_COMPOSE_DIR/docker-compose.yml"
 export KAFKA_COMPOSE_FILE="$KAFKA_COMPOSE_DIR/docker-compose.yml"
